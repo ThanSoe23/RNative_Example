a85a800a353baf0cd95ee9f1e22e768ec3bf4a3b
ThanSoe23 <100922541+ThanSoe23@users.noreply.github.com>
2022-05-09 11:16:37 +0630

    first_example

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.body}>
+        <TextInput
+          placeholder="Your course goal!"
+          style={styles.appContainer}
+        />
+        <View style={styles.btn}>
+          <Button title="Add Goal" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    paddingTop: 80,
+    paddingHorizontal: 10,
+    alignItems: "center",
+  },
+
+  body: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "start",
+  },
+
+  btn: {
+    height: 50,
+    marginLeft: 10,
+    paddingHorizontal: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+  },
+
+  appContainer: {
+    height: 50,
+    width: 250,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "indigo",
+    borderRadius: 4,
   },
 });
